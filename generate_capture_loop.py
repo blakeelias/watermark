3c9a1006feede433e3d04b3614ff2aa13eeb99c2
Blake Elias <blakeelias@mit.edu>
2024-07-14 21:17:15 +0000

    Generate files on host -> display on Raspberry Pi -> capture camera image on Raspberry Pi -> send captured image back to host

--- generate_capture_loop.py
+++ generate_capture_loop.py
@@ -3,10 +3,15 @@ import generate_hashed_images
 import time
 import os
 
+SSH_USER = "guest"
+SSH_HOST = "rpi5.local"
+
 
 def main():
+  image_path = '/tmp/watermark_noise'
   image_paths, expected_texts = generate_hashed_images.generate_qr_codes(
-      '/tmp/watermark_noise', 16)
+      image_path, 16)
+  os.system(f'scp -r {image_path} {SSH_USER}@{SSH_HOST}:{image_path}')
 
   for i, (image_path,
           expected_text) in enumerate(zip(image_paths, expected_texts)):
@@ -29,15 +34,20 @@ def main():
 def display_image(image_path: str) -> None:
   # Display image_path
   print(f'Displaying {image_path}')
-  os.system(f'feh {image_path}')
+  cmd = f'feh {image_path}'
+  os.system(f'ssh {SSH_USER}@{SSH_HOST} {cmd}')
 
 
 def capture_image(image_save_path: str) -> None:
   print(f'Capturing camera image; saving to {image_save_path}')
   # Capture image from camera
-  os.system('libcamera-vid -t 100 -o /tmp/video_capture.mp4')
+  cmd = 'libcamera-vid -t 100 -o /tmp/video_capture.mp4'
+  os.system(f'ssh {SSH_USER}@{SSH_HOST} {cmd}')
+
   # Extract a single frame from the video and save it as a PNG image
-  os.system('ffmpeg -i /tmp/video_capture.mp4 -vframes 1 {image_save_path}')
+  cmd = f'ffmpeg -i /tmp/video_capture.mp4 -vframes 1 {image_save_path}'
+  os.system(f'ssh {SSH_USER}@{SSH_HOST} {cmd}')
+  os.system(f'scp {SSH_USER}@{SSH_HOST}:{image_save_path} {image_save_path}')
 
 
 if __name__ == "__main__":
