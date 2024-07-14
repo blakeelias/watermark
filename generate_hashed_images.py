f6d7b11147778b3d346c78818a12fc84b8c0f7a4
Blake Elias <blakeelias@mit.edu>
2024-07-14 22:20:04 +0000

    Generate random checkerboard patterns using date as random seed

--- generate_hashed_images.py
+++ generate_hashed_images.py
@@ -1,9 +1,10 @@
 import qrcode
-from PIL import ImageOps
+from PIL import ImageOps, Image
 import os
 import time
 import hashlib
 from typing import List, Tuple
+import numpy as np
 
 
 def generate_qr_codes(image_path: str,
@@ -44,6 +45,45 @@ def generate_qr_codes(image_path: str,
         print(f"Generated QR code {i+1}/{num_images}")
 
     return filenames, expected_texts
+
+
+def generate_checkerboards(
+    image_path: str,
+    num_images: int,
+    checkerboard_dims: Tuple[int, int] = (4, 4)
+) -> Tuple[List[str], List[np.array]]:
+    '''
+    Generate a list of random checkerboard patterns.
+    Returns:
+        A list of the image paths.
+    '''
+    os.makedirs(image_path, exist_ok=True)
+    filenames = []
+    expected_checkerboards = []
+
+    for i in range(num_images):
+        # Get the current timestamp and hash it
+        timestamp = str(time.time()).encode('utf-8')
+        print(timestamp)
+        hashed_timestamp = hashlib.sha256(timestamp).hexdigest()
+        np.radom.seed(hashed_timestamp)
+
+        # Create random checkerboard pattern using datetime as a seed
+        checkerboard = np.random.randint(0, 2, size=checkerboard_dims)
+
+        # Convert checkerboard pattern to an image
+        img = Image.fromarray(checkerboard)
+
+        # Save the image
+        filename = f"{image_path}/checkerboard_{i+1}.png"
+        img.save(filename)
+
+        filenames.append(filename)
+        expected_checkerboards.append(checkerboard)
+
+        print(f"Generated QR code {i+1}/{num_images}")
+
+    return filenames, expected_checkerboards
 
 
 if __name__ == "__main__":
